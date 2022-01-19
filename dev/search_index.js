98f027e5110261392c5e70c2acf3c478c7896016
Documenter.jl <documenter@juliadocs.github.io>
2022-01-19 10:02:19 +0000

    build based on f718680

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DenoiseDNA","category":"page"},{"location":"#DenoiseDNA","page":"Home","title":"DenoiseDNA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DenoiseDNA.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DenoiseDNA]","category":"page"}]
+}
