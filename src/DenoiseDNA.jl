a6a20f9f7f3cbd37548840e541f9e6020dde320c
Arthur <arn203@exeter.ac.uk>
2022-01-20 11:46:28 +0000

    initial files

--- src/DenoiseDNA.jl
+++ src/DenoiseDNA.jl
@@ -1,5 +1,20 @@
 module DenoiseDNA
+using FASTX, BioSequences, LoopVectorization, CodecZlib, BioAlignments, DataFrames#, CSV,ProfileView
+using LazyStack, Poppler_jll,StatsBase#,FastHistograms,StatsBase,ProfileView#,CairoMakie
+using RCall#, ReadDataStores
+#using StatsFuns, SortingLab, Tullio,Polyester,UnicodePlots
+using StatsFuns, Loess,CairoMakie#, MultipleTesting, SparseArrays
+
+include.(["plotting.jl",
+        "fileIO.jl",
+        "kmers.jl",
+        "DADA.jl"])
 
 # Write your package code here.
-
+export quality_plot,
+        dada,
+        custering,
+        trim,
+        dereplicate,
+        get_files
 end
